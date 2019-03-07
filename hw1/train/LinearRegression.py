65270cadd3ac53452fde17fb6836dfc35b7d27a2
hhccode <ms718293@gmail.com>
2019-03-07 21:10:41 +0800

    ml hw1

---
+++ hw1/train/LinearRegression.py
@@ -0,0 +1,106 @@
+import numpy as np
+
+class LinearRegression():
+    
+    def __init__(self, X, y):
+        self.X = X
+        self.y = y
+        self.optimize = {"Adagrad": self._Adagrad, "Adam": self._Adam, "ClosedForm": self._ClosedForm}
+
+    def GradientDescent(self, optimizer="ClosedForm", **kwargs):
+        return self.optimize[optimizer](**kwargs)
+
+    def _ClosedForm(self):
+        size, dim = self.X.shape
+        
+        # Add the bias term
+        X = []
+        for r in range(size):
+            X.append(np.append([1], self.X[r]))
+        X = np.array(X)
+        
+        # Closed form solution
+        from numpy.linalg import pinv
+        w = np.matmul(pinv(X), self.y)
+        
+        predict_y = np.dot(X, w)
+        loss = predict_y - self.y
+        
+        print("Iteration: 1")
+        print(self._GetRMSE(loss, size))
+
+        return w
+    
+    def _Adagrad(self, lr=1000, epoch=100000, lambda_=0):
+        size, dim = self.X.shape
+        print(lambda_)
+        b = 0.0
+        w = np.zeros(dim)
+        b_GRAD = 0.0
+        w_GRAD = np.zeros(dim)
+
+        for i in range(epoch):
+            predict_y = np.dot(self.X, w) + b
+            loss = predict_y - self.y
+
+            b_grad = 2 * np.sum(loss)
+            w_grad = 2 * np.dot(self.X.T, loss) + lambda_ * 2 * w
+            
+            b_GRAD += b_grad ** 2
+            w_GRAD += w_grad ** 2
+        
+            b -= lr * b_grad / (np.sqrt(b_GRAD) + 0.0005)
+            w -= lr * w_grad / (np.sqrt(w_GRAD) + 0.0005)
+
+            if i % 500 == 0:
+                print("Iteration=", i)
+                print(self._GetRMSE(loss, size))
+                
+        print("Iteration=", epoch)
+        print(self._GetRMSE(loss, size))
+
+        return np.append(b, w)
+        
+
+    def _Adam(self, lr=0.0001, epoch=10000, beta_one=0.9, beta_two=0.999, epsilon=1e-8, lambda_=0):
+        size, dim = self.X.shape
+
+        b = 0.0
+        w = np.zeros(dim)
+        
+        Vb = 0.0    # Momentum for b
+        Sb = 0.0    # RMSprop for b
+        Vw = np.zeros(dim)   # Momentum for w
+        Sw= np.zeros(dim)   # RMSprop   for w
+        
+        for i in range(epoch):
+            predict_y = np.dot(self.X, w) + b
+            loss = predict_y - self.y
+            
+            b_grad = 2 * np.sum(loss)
+            w_grad = 2 * np.dot(self.X.T, loss) + lambda_ * 2 * w
+
+            Vb = beta_one * Vb + (1 - beta_one) * b_grad
+            Sb = beta_two * Sb + (1 - beta_two) * (b_grad ** 2)
+            Vw = beta_one * Vw + (1 - beta_one) * w_grad
+            Sw = beta_two * Sw + (1 - beta_two) * (w_grad ** 2)
+
+            Vb_hat = Vb / (1 - pow(beta_one, i+1))
+            Sb_hat = Sb / (1 - pow(beta_two, i+1))
+            Vw_hat = Vw / (1 - pow(beta_one, i+1))
+            Sw_hat = Sw / (1 - pow(beta_two, i+1))
+
+            b -= lr * Vb_hat / (np.sqrt(Sb_hat) + epsilon)
+            w -= lr * Vw_hat / (np.sqrt(Sw_hat) + epsilon)
+            
+            if i % 500 == 0:
+                print("Iteration=", i)
+                print(self._GetRMSE(loss, size))
+
+        print("Iteration=", epoch)
+        print(self._GetRMSE(loss, size))
+
+        return np.append(b, w)
+
+    def _GetRMSE(self, loss, data_size):
+        return np.sqrt(np.sum(loss ** 2) / data_size)
---
+++ hw1/train/hw1_2.py
@@ -0,0 +1,124 @@
+import pandas as pd
+import numpy as np
+from LinearRegression import LinearRegression
+
+
+ALL_FEATURES = {"AMB_TEMP": 0, "CH4": 1, "CO": 2, "NMHC": 3, "NO": 4,
+                "NO2": 5, "NOx": 6, "O3": 7, "PM10": 8, "PM2.5": 9,
+                "RAINFALL": 10, "RH": 11, "SO2": 12, "THC": 13, "WD_HR": 14,
+                "WIND_DIREC": 15, "WIND_SPEED": 16, "WS_HR": 17}
+
+def to_float(x):
+    if x == "NR":
+        return 0.0
+    else:
+        return float(x)
+
+def find_dirty(array):
+    for i in range(len(array)):
+        if array[i] < 0.0:
+            return True
+
+    return False
+
+def extract_features(wanted_features, hour=9, nomorlized=True):
+    # Read the training file
+    training_df = pd.read_csv('train.csv', encoding='big5').values
+    
+    # Declare the 18-dim vector & extract 162 features
+    training_data = [[] for _ in range(18)]
+    for i in range(len(training_df)):
+        training_data[i%18].extend(list(map(to_float, training_df[i][3:])))
+    training_data = np.array(training_data)
+    
+    row, col = training_data.shape
+    temp_X, training_y = [], []       
+    
+    cnt = 0
+    i = 0
+    while i < col-hour:
+        X = training_data[:, i:i+hour].flatten()
+        y = training_data[9, i+hour]
+        
+        temp_X.append(X)
+        training_y.append(y)
+        i += 1
+
+    temp_X = np.array(temp_X)
+    training_y= np.array(training_y)
+    
+    # Normalize the data
+    if nomorlized:
+        MEAN = np.mean(temp_X, axis=0)
+        STD = np.std(temp_X, axis=0)
+        for r in range(temp_X.shape[0]):
+            for c in range(temp_X.shape[1]):
+                if not STD[c] == 0 :
+                    temp_X[r][c] = (temp_X[r][c]- MEAN[c]) / STD[c]
+    
+    training_X = [[] for _ in range(temp_X.shape[0])]
+    for r in range(temp_X.shape[0]):
+        for power in range(len(wanted_features)):
+            for wanted_feature in wanted_features[power]:
+                training_X[r].extend(temp_X[r, hour*ALL_FEATURES[wanted_feature]:hour*(ALL_FEATURES[wanted_feature]+1)] ** (power+1))
+        
+    training_X = np.array(training_X)
+
+    # Read the testing file
+    testing_df = pd.read_csv('test.csv', encoding='big5', header=None).values
+
+    # Store the testing date as the floating number
+    testing_data = []
+    for i in range(len(testing_df)):
+        testing_data.append(list(map(to_float, testing_df[i, 2+(9-hour):])))
+    testing_data = np.array(testing_data)
+
+    row, col = testing_data.shape
+    
+    temp_X = []
+    
+    for i in range(0, row, 18):
+        temp_X.append(testing_data[i:i+18, :].flatten())
+    temp_X = np.array(temp_X)
+    
+    if nomorlized:
+        for r in range(temp_X.shape[0]):
+            for c in range(temp_X.shape[1]):
+                if not STD[c] == 0 :
+                    temp_X[r][c] = (temp_X[r][c]- MEAN[c]) / STD[c]
+
+    testing_X = [[1] for r in range(temp_X.shape[0])]
+    for r in range(temp_X.shape[0]):
+        for power in range(len(wanted_features)):
+            for wanted_feature in wanted_features[power]:
+                testing_X[r].extend(temp_X[r, hour*ALL_FEATURES[wanted_feature]:hour*(ALL_FEATURES[wanted_feature]+1)] ** (power+1))
+        
+    testing_X = np.array(testing_X)
+    
+    return training_X, training_y, testing_X
+    
+
+
+if __name__ == "__main__":
+    
+    '''
+    features = [["AMB_TEMP", "CH4", "CO", "NMHC", "NO",
+                "NO2", "NOx", "O3", "PM10", "PM2.5",
+                "RAINFALL", "RH", "SO2", "THC", "WD_HR",
+                "WIND_DIREC", "WIND_SPEED", "WS_HR"]]
+    
+    '''
+    features = [["PM2.5"]]
+    
+    training_x, training_y, testing_x = extract_features(wanted_features=features, hour=5, nomorlized=True)
+    
+    model = LinearRegression(training_x, training_y)
+    
+    weight = model.GradientDescent(lr=0.01, epoch=20000, optimizer="Adam")
+
+    testing_y = np.dot(testing_x, weight)
+
+    with open("output.csv", "w") as f:
+        print("id,value", file=f)
+        for i in range(testing_y.shape[0]):
+            print("id_{},{}".format(i, testing_y[i]), file=f)
